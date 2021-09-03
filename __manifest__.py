0e9cb53288de3a2b009aba7cfcdc240d8403e331
TCP-TDF <88446276+InformaticaTCP@users.noreply.github.com>
2021-09-03 14:41:19 -0300

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+
+{
+    'name': "TCP Lista de Organismos de Tierra del Fuego",
+    'version': '0.1',
+    'category': 'TCP',
+    'summary': """
+        Módulo para obtener un listado de Organismos de Tierra del Fuego
+    """,
+    'description': """
+        Módulo para obtener un listado de Organismos de Tierra del Fuego
+    """,
+    'author': "Carlos Vargas -Informatica y Comunicaciones",
+    'maintainer': "Informática y Comunicaciones",
+    'website': "http://www.tcptdf.gob.ar",
+    'license': 'LGPL-3',
+    'depends': [
+        'base'
+        ],
+    'data': [
+    ],
+    'application': True,
+    'installable': True,
+}
