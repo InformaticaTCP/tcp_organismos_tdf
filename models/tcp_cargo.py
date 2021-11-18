3e4deb6bb3f3cc2a29f842cdf688377aeea0be3a
cvargastcp <cvargas@tcptdf.gob.ar>
2021-11-18 10:23:37 -0300

    sistema finalizado

--- models/tcp_cargo.py
+++ models/tcp_cargo.py
@@ -14,4 +14,4 @@ class TcpCargo(models.Model):
     nombre_organismo = fields.Char(related='area_id.organismo_id.name', string='Organismo') 
     # Relaciones entre Areas y cargos 
     area_id = fields.Many2one(string='Area', comodel_name='tcp.area', ondelete='restrict')
-    organismo = fields.Many2one(string='Que organismo', related=('area_id.organismo_id')
+    organismo = fields.Many2one(string='Organismo', related=('area_id.organismo_id'))
